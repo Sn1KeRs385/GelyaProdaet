14389c69b536cc34f8c170e292fb4d697bebe09d
Dmitriy <dima.385@mail.ru>
2023-03-21 00:59:47 +0500

    fix browser versions

--- www/admin/postcss.config.js
+++ www/admin/postcss.config.js
@@ -8,14 +8,15 @@ module.exports = {
     // https://github.com/postcss/autoprefixer
     require('autoprefixer')({
       overrideBrowserslist: [
-        'last 4 Chrome versions',
-        'last 4 Firefox versions',
-        'last 4 Edge versions',
-        'last 4 Safari versions',
-        'last 4 Android versions',
-        'last 4 ChromeAndroid versions',
-        'last 4 FirefoxAndroid versions',
-        'last 4 iOS versions',
+        "last 10 Chrome versions",
+        "last 10 Firefox versions",
+        "last 4 Edge versions",
+        "last 7 Safari versions",
+        "last 8 Android versions",
+        "last 8 ChromeAndroid versions",
+        "last 8 FirefoxAndroid versions",
+        "last 10 iOS versions",
+        "last 5 Opera versions"
       ],
     }),
 
