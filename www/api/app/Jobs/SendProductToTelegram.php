7746a03f44a1f6146df0010af9c772ad3803d076
Dmitriy <dima.385@mail.ru>
2023-03-21 05:03:37 +0500

    fix

--- www/api/app/Jobs/SendProductToTelegram.php
+++ www/api/app/Jobs/SendProductToTelegram.php
@@ -18,6 +18,7 @@ class SendProductToTelegram implements ShouldQueue
 
     public function __construct(protected Product $product)
     {
+        $this->productService = app(ProductService::class);
     }
 
     public function handle(): void
