1ac3f5cd0565f602892e4f39c4a03d86487c4702
Dmitriy <dima.385@mail.ru>
2023-03-21 20:29:55 +0500

    up uploader version

--- www/api/config/uploader_collection_settings.php
+++ www/api/config/uploader_collection_settings.php
@@ -5,13 +5,17 @@ return [
         'queue' => 'file-converter',
         'disk' => 's3',
         'options' => [
-            'optimize_if_size_more_mb' => 4.9,
+            'optimize_if_size_more' => 5,
+            'zip_to' => 5,
         ],
         'jobs' => [
-            \Sn1KeRs385\FileUploader\App\Jobs\ImageToWebpAndOptimizationConverter::class,
+            \Sn1KeRs385\FileUploader\App\Jobs\StartConverting::class,
             \Sn1KeRs385\FileUploader\App\Jobs\FileConverter::class,
+            \Sn1KeRs385\FileUploader\App\Jobs\ImageOptimize::class,
+            \Sn1KeRs385\FileUploader\App\Jobs\ImageZipToSize::class,
             \Sn1KeRs385\FileUploader\App\Jobs\ExtractMetaDataFromFile::class,
             \Sn1KeRs385\FileUploader\App\Jobs\ExtractMetaDataFromImage::class,
+            \Sn1KeRs385\FileUploader\App\Jobs\EndConverting::class,
         ],
     ],
 ];
