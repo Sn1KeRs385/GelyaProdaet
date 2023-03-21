04e83d74177d0b7a56bb5df9b8bf328ca05dc1f4
Dmitriy <dima.385@mail.ru>
2023-03-21 07:07:35 +0500

    try delay

--- www/api/app/Observers/ProductObserver.php
+++ www/api/app/Observers/ProductObserver.php
@@ -9,11 +9,11 @@ class ProductObserver
 {
     public function created(Product $product): void
     {
-        SendProductToTelegram::dispatch($product)->onQueue('tg_public_messages')->afterCommit();
+        SendProductToTelegram::dispatch($product)->onQueue('tg_public_messages')->delay(now()->addSeconds(10));
     }
 
     public function updated(Product $product): void
     {
-        SendProductToTelegram::dispatch($product)->onQueue('tg_public_messages')->afterCommit();
+        SendProductToTelegram::dispatch($product)->onQueue('tg_public_messages')->delay(now()->addSeconds(10));
     }
 }
