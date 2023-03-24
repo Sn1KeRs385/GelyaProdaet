f6e66621a9c72e9e502b7e43ab82cd349ba46424
Dmitriy <dima.385@mail.ru>
2023-03-24 16:29:15 +0500

    Change tg send logic and text

--- www/api/app/Services/ProductService.php
+++ www/api/app/Services/ProductService.php
@@ -14,9 +14,6 @@ class ProductService
 {
     public function sendProductToTelegram(Product $product, string|int $chatId = null): void
     {
-        if (!$product->items()->where('is_for_sale', true)->exists()) {
-            return;
-        }
         if (!$chatId) {
             if (is_numeric(config('telegram.public_id'))) {
                 $chatId = config('telegram.public_id');
@@ -39,33 +36,37 @@ class ProductService
                 'message_id' => $message->message_id,
                 'caption' => $this->getTgMessageText($product),
             ]);
-        } else {
-            $inputMediaPhoto = [];
-            foreach ($product->files as $file) {
-                $photo = new InputMediaPhoto();
-                $photo->type = 'photo';
-                $photo->media = $file->url;
-//                $photo->media = 'https://shop-cdn1.vigbo.tech/shops/173204/products/20950271/images/3-2efe41fcd3f4c89971fc7bd505632f89.jpg?version=1';
-                if (count($inputMediaPhoto) === 0) {
-                    $photo->caption = $this->getTgMessageText($product);
-                } else {
-                    $photo->caption = '';
-                }
-                $photo->parse_mode = ParseMode::HTML;
-                $photo->has_spoiler = false;
-                $inputMediaPhoto[] = $photo;
-            }
-
-            /** @var Message[] $response */
-            $response = $bot->sendMediaGroup($inputMediaPhoto, [
-                'chat_id' => $chatId,
-            ]);
-            $product->tgMessages()->create([
-                'chat_id' => $chatId,
-                'message_id' => $response[0]->message_id,
-                'file_ids' => $product->files->pluck('id'),
-            ]);
+            return;
         }
+
+        if (!$product->items()->where('is_for_sale', true)->exists()) {
+            return;
+        }
+
+        $inputMediaPhoto = [];
+        foreach ($product->files as $file) {
+            $photo = new InputMediaPhoto();
+            $photo->type = 'photo';
+            $photo->media = $file->url;
+            if (count($inputMediaPhoto) === 0) {
+                $photo->caption = $this->getTgMessageText($product);
+            } else {
+                $photo->caption = '';
+            }
+            $photo->parse_mode = ParseMode::HTML;
+            $photo->has_spoiler = false;
+            $inputMediaPhoto[] = $photo;
+        }
+
+        /** @var Message[] $response */
+        $response = $bot->sendMediaGroup($inputMediaPhoto, [
+            'chat_id' => $chatId,
+        ]);
+        $product->tgMessages()->create([
+            'chat_id' => $chatId,
+            'message_id' => $response[0]->message_id,
+            'file_ids' => $product->files->pluck('id'),
+        ]);
     }
 
     protected function getTgMessageText(Product $product): string
@@ -77,6 +78,13 @@ class ProductService
             ->with(['size', 'color'])
             ->where('is_for_sale', true)
             ->get();
+
+        if (count($items) === 0) {
+            /** @var ProductItem[] $items */
+            $items = $product->items()
+                ->with(['size', 'color'])
+                ->get();
+        }
 
         if (count($items) === 0) {
             throw new \Exception('ProductItem is empty to send');
@@ -95,7 +103,7 @@ class ProductService
                     'colors' => [],
                 ];
             }
-            if ($item->is_sold) {
+            if ($item->is_sold || !$item->is_for_sale) {
                 $sizes[$item->size->title]['is_sold'] = $sizes[$item->size->title]['is_sold'] + 1;
             } else {
                 $sizes[$item->size->title]['for_sale'] = $sizes[$item->size->title]['for_sale'] + 1;
@@ -117,7 +125,7 @@ class ProductService
             $text .= " ({$product->country->title})";
         }
 
-        $text .= "\n{$product->title}";
+//        $text .= "\n{$product->title}";
 
         if ($product->description) {
             $text .= "\n$product->description";
